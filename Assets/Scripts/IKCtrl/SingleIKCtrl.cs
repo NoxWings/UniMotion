eb95af18f692b514881a4dc363f506ae0ab00ce0
David G. Miguel <noxwings@gmail.com>
2014-03-31 13:51:58 +0200

    Simple IK control script added
    Added a helper script that enables humanoid inverse kinematics to be bound to a tranform.

---
+++ Assets/Scripts/IKCtrl/SingleIKCtrl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class SingleIKCtrl : MonoBehaviour {
+
+	protected Animator animator;
+	public bool activateIK = false;
+	public AvatarIKGoal avatarIK = AvatarIKGoal.RightHand;
+	public Transform goalTarget = null;
+
+	void Awake() {
+		animator = this.GetComponent<Animator>();
+	}
+
+	void OnAnimatorIK()
+	{
+		if (animator != null) {
+			if (activateIK && goalTarget != null) {
+				setIKWeight(1.0f);
+				setIKTransform(goalTarget);
+			} else {
+				setIKWeight(0f);
+			}
+		}
+	}
+
+	void setIKWeight(float weight) {
+		animator.SetIKPositionWeight(avatarIK, weight);
+		animator.SetIKRotationWeight(avatarIK, weight);
+	}
+
+	void setIKTransform(Transform goal) {
+		animator.SetIKPosition(avatarIK, goal.position);
+		animator.SetIKRotation(avatarIK, goal.rotation);
+	}
+}
+
